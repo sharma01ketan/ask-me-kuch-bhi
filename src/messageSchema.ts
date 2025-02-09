3fabf3a641a0045fa1dac2a0925f84a941a60a9f
Ketan Sharma <sharma01ketan@gmail.com>
2025-02-09 14:48:53 +0530

    deletion

--- src/messageSchema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod"
-
-export const messageSchema = z.object({
-  content: z
-          .string()
-          .min(10,{message: "Content must be at least 10 characters"})
-          .min(3000,{message: "Content must be no longer than 300 characters"})
-
-})
