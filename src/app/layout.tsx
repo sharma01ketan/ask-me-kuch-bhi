c5d7390a7e72d4e337ba0bb0971ce381094b8403
Ketan Sharma <sharma01ketan@gmail.com>
2025-02-26 22:37:59 +0530

    trying-clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import AuthProvider from '../context/AuthProvider';
-import { Toaster } from '@/components/ui/toaster';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+// import AuthProvider from "../context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'True Feedback',
-  description: 'Real feedback from real people.',
+  title: "True Feedback",
+  description: "Real feedback from real people.",
 };
 
 interface RootLayoutProps {
@@ -17,14 +18,13 @@ interface RootLayoutProps {
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" >
-      <AuthProvider>
+    <html lang="en">
+      <ClerkProvider>
         <body className={inter.className}>
           {children}
           <Toaster />
         </body>
-      </AuthProvider>
+      </ClerkProvider>
     </html>
   );
 }
-
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,67 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in",
+  "/sign-up",
+  "/verify",
+  "/",
+]);
+
+const isPublicApiRoute = createRouteMatcher([]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const userId = (await auth()).userId;
+  const currentUrl = new URL(req.url);
+  const isAccessingDashboard = currentUrl.pathname === "/dashboard";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!userId) {
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
 };
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  const url = request.nextUrl;
+// import { NextRequest, NextResponse } from 'next/server';
+// import { getToken } from 'next-auth/jwt';
+// export { default } from 'next-auth/middleware';
 
-  // Redirect to dashboard if the user is already authenticated
-  // and trying to access sign-in, sign-up, or home page
-  if (
-    token &&
-    (url.pathname.startsWith('/sign-in') ||
-      url.pathname.startsWith('/sign-up') ||
-      url.pathname.startsWith('/verify') ||
-      url.pathname === '/')
-  ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
+// export const config = {
+//   matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+// };
 
-  if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
-  }
+// export async function middleware(request: NextRequest) {
+//   const token = await getToken({ req: request });
+//   const url = request.nextUrl;
 
-  return NextResponse.next();
-}
+//   // Redirect to dashboard if the user is already authenticated
+//   // and trying to access sign-in, sign-up, or home page
+//   if (
+//     token &&
+//   (url.pathname.startsWith('/sign-in') ||
+//     url.pathname.startsWith('/sign-up') ||
+//     url.pathname.startsWith('/verify') ||
+//     url.pathname === '/')
+// ) {
+//     return NextResponse.redirect(new URL('/dashboard', request.url));
+//   }
+
+//   if (!token && url.pathname.startsWith('/dashboard')) {
+//     return NextResponse.redirect(new URL('/sign-in', request.url));
+//   }
+
+//   return NextResponse.next();
+// }
